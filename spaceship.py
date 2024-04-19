a897e38aa91c1067acb5c60d82156e05cfca9011
OscarWetzel <oscarwetzel@web.de>
2024-04-19 17:59:20 +0200

    2 Player StarWarsflight

--- spaceship.py
+++ spaceship.py
@@ -17,6 +17,7 @@ gem.x = random.randint(20, 780)
 gem.y = 0
 
 score = 0
+score2 = 0
 game_over = False
 
 
@@ -25,40 +26,60 @@ def on_mouse_move(pos, rel, buttons):
 
 
 def update():
-    global score, game_over
+    global score, score2, game_over
 
     if keyboard.left:
-        ship.x = ship.x - 5
+        ship.x = ship.x - 10
     if keyboard.right:
-        ship.x = ship.x + 5
+        ship.x = ship.x + 10
 
     if keyboard.a:
-        ship2.x = ship2.x - 5
+        ship2.x = ship2.x - 10
     if keyboard.d:
-        ship2.x = ship2.x + 5
+        ship2.x = ship2.x + 10
 
     gem.y = gem.y + 4 + score / 5
     if gem.y > 600:
         game_over = True
+
+    if gem.colliderect(ship2) and gem.colliderect(ship):
+        gem.x = random.randint(20, 780)
+        gem.y = 0
+        score2 = score2 + 1
+        score = score + 1
+
     if gem.colliderect(ship):
         gem.x = random.randint(20, 780)
         gem.y = 0
         score = score + 1
 
+    if gem.colliderect(ship2):
+        gem.x = random.randint(20, 780)
+        gem.y = 0
+        score2 = score2 + 1
+
+
+
 
 def draw():
+    global game_over, score, score2
     screen.fill((80, 0, 70))
     if game_over:
         screen.draw.text("Game Over", (360, 300), color=(255, 255, 255), fontsize=60)
         screen.draw.text(
-            "Score: " + str(score), (360, 350), color=(255, 255, 255), fontsize=60
+            "Score: " + str(score + score2), (360, 350), color=(255, 255, 255), fontsize=60
         )
+        if keyboard.r:
+            score = 0
+            score2 = 0
+            gem.y = 0
+            game_over = False
     else:
         gem.draw()
         ship.draw()
         ship2.draw()
         screen.draw.text(
-            "Score: " + str(score), (15, 10), color=(255, 255, 255), fontsize=30
+            "Score Blue: " + str(score) + "\n" + "Score Green: " + str(score2), (15, 10), color=(150, 150, 255), fontsize=30
         )
 
 
